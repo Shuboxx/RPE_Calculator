001c450ebe4be8eb090472aa24cf0f5e9f657278
Patrick Bloniarz <pbloniarz@protonmail.com>
2021-07-22 11:50:43 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// let weightLifted = Number(document.querySelector('#weight').value)
+// let repsLifted = Number(document.querySelector('#reps').value)
+let maxWeight = document.querySelector('#output')
+let calc = document.querySelector('#calculate')
+
+calc.addEventListener('click', calcMaxWeight)
+
+function round5(weight) {
+  return Math.round(weight / 5) * 5
+}
+
+function calcMaxWeight(weight, reps) {
+  weight = Number(document.querySelector('#weight').value)
+  reps = Number(document.querySelector('#reps').value)
+  if(reps === 1) {
+    console.log(weight)
+  } else {
+    let estimatedMaxWeight = weight * (1 + (reps / 30))
+    console.log(Math.floor(estimatedMaxWeight / 5) * 5)
+  }
+}
+// weight(1 + reps / 30)
