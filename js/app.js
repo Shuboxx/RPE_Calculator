b1c0c74123803fb540dd255240cf687833153e42
Patrick Bloniarz <pbloniarz@protonmail.com>
2021-07-25 20:10:25 -0700

    Added calculations for rpe ranges from 6 through 10 with 1 rep

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
-// let weightLifted = Number(document.querySelector('#weight').value)
-// let repsLifted = Number(document.querySelector('#reps').value)
-let maxWeight = document.querySelector('#output')
-let calc = document.querySelector('#calculate')
+let maxWeight = document.querySelector("#output")
+let calc = document.querySelector("#calculate")
 
-calc.addEventListener('click', calcMaxWeight)
+calc.addEventListener("click", calcMaxWeight)
+calc.addEventListener("click", calculateRPEWeights)
 
+// Calculates estimated 1 rep max
 function calcMaxWeight(weight, reps) {
-  weight = Number(document.querySelector('#weight').value)
-  reps = Number(document.querySelector('#reps').value)
-  if(reps === 1) {
+  weight = Number(document.querySelector("#weight").value)
+  reps = Number(document.querySelector("#reps").value)
+  if (reps === 1) {
     maxWeight.innerText = `Estimated 1rm: ${weight}`
+    return weight
   } else {
-    let estimatedMaxWeight = weight * (1 + (reps / 30))
-    let roundedMax = (Math.floor(estimatedMaxWeight / 5) * 5)
+    let estimatedMaxWeight = weight * (1 + reps / 30)
+    let roundedMax = Math.ceil(estimatedMaxWeight / 5) * 5
     maxWeight.innerText = `Estimated 1rm: ${roundedMax}`
+    return roundedMax
   }
 }
-// weight(1 + reps / 30)
+
+// Calculates weight based on RPE
+function calculateRPEWeights(maxWeight) {
+  maxWeight = calcMaxWeight()
+  let rpe = Number(document.querySelector("#rpe").value)
+  switch (rpe) {
+    case 10:
+      document.querySelector("#oneTen").innerText = maxWeight * 1
+      break
+    case 9:
+      document.querySelector("#oneNine").innerText =
+        Math.round((maxWeight * 0.955) / 5) * 5
+      break
+    case 8:
+      document.querySelector("#oneEight").innerText =
+        Math.round((maxWeight * 0.929) / 5) * 5
+      break
+    case 7:
+      document.querySelector("#oneSeven").innerText =
+        Math.round((maxWeight * 0.892) / 5) * 5
+      break
+    case 6:
+      document.querySelector("#oneSix").innerText =
+        Math.round((maxWeight * 0.863) / 5) * 5
+  }
+}
+
+// Calculates weight based on RPE
